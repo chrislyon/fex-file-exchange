a009abefadef841c3c7d6ab03914ba76309ca50a
chris <chris@localhost>
2012-11-14 18:58:15 +0100

    Les bases du modele de donnees avec qq exemples

--- fex/src/main.py
+++ fex/src/main.py
@@ -4,78 +4,114 @@ from elixir import *
 from model import *
 import datetime
 
-def pop_entite():
-    ## SRA la racine
-    e = Entite()
-    e.codent = "SRA"
-    e.nom = "SRA INFORMATIQUE"
-    e.typ = 'ROOT'
-    session.commit()
+class Hub(object):
+    def __init__(self, name):
+        self.name  = name
 
-    ## Un Client 
-    e = Entite()
-    e.codent = "CLIENT1"
-    e.nom = "CLIENT No 1"
-    e.typ = 'CLIENT'
-    session.commit()
+    def pop_entite(self):
+        ## SRA la racine
+        e = Entite()
+        e.codent = u"SRA"
+        e.nom = u"SRA INFORMATIQUE"
+        e.typ = u'ROOT'
+        session.commit()
 
-    ## Un autre client
-    e = Entite()
-    e.codent = "CLIENT2"
-    e.nom = "CLIENT No 2"
-    e.typ = 'CLIENT'
-    session.commit()
+        ## Un Client 
+        e = Entite()
+        e.codent = u"CLIENT1"
+        e.nom = u"CLIENT No 1"
+        e.typ = u'CLIENT'
+        session.commit()
 
-def pop_user():
-    SRA = Entite.get_by(codent="SRA")
-    ## Admin SRA
-    u = User()
-    u.codusr = "SRA_ADM1"
-    u.name = "Admin SRA"
-    u.entite = SRA
-    u.IsAdmin = True
-    session.commit()
-    ## User CLIENT1
-    CLI1 = Entite.get_by(codent="CLIENT1")
-    u = User()
-    u.codusr = "CLI1_ADM"
-    u.name = "Admin CLIENT1"
-    u.entite = CLI1
-    u.IsAdmin = True
-    session.commit()
-    u = User()
-    u.codusr = "CLI1_USR1"
-    u.name = "USER1 CLIENT1"
-    u.entite = CLI1
-    u.IsAdmin = False
-    session.commit()
-    u = User()
-    u.codusr = "CLI1_USR2"
-    u.name = "USER2 CLIENT1"
-    u.entite = CLI1
-    u.IsAdmin = False
-    session.commit()
-    ## User CLIENT2
-    CLI2 = Entite.get_by(codent="CLIENT2")
-    u = User()
-    u.codusr = "CLI2_ADM"
-    u.name = "Admin CLIENT2"
-    u.entite = CLI2
-    u.IsAdmin = True
-    session.commit()
-    u = User()
-    u.codusr = "CLI2_USR2"
-    u.name = "USER2 CLIENT2"
-    u.entite = CLI2
-    u.IsAdmin = False
-    session.commit()
+        ## Un autre client
+        e = Entite()
+        e.codent = u"CLIENT2"
+        e.nom = u"CLIENT No 2"
+        e.typ = u'CLIENT'
+        session.commit()
 
-def populate():
-    pop_entite()
-    pop_user()
-    #pop_fex()
+    def pop_user(self):
+        SRA = Entite.get_by(codent=u"SRA")
+        ## Admin SRA
+        u = User()
+        u.codusr = u"SRA_ADM1"
+        u.name = u"Admin SRA"
+        u.entite = SRA
+        u.IsAdmin = True
+        session.commit()
+        ## User SRA
+        u = User()
+        u.codusr = u"SRA_USR1"
+        u.name = u"USER SRA"
+        u.entite = SRA
+        u.IsAdmin = False
+        session.commit()
+        ## User CLIENT1
+        CLI1 = Entite.get_by(codent=u"CLIENT1")
+        u = User()
+        u.codusr = u"CLI1_ADM"
+        u.name = u"Admin CLIENT1"
+        u.entite = CLI1
+        u.IsAdmin = True
+        session.commit()
+        u = User()
+        u.codusr = u"CLI1_USR1"
+        u.name = u"USER1 CLIENT1"
+        u.entite = CLI1
+        u.IsAdmin = False
+        session.commit()
+        u = User()
+        u.codusr = u"CLI1_USR2"
+        u.name = u"USER2 CLIENT1"
+        u.entite = CLI1
+        u.IsAdmin = False
+        session.commit()
+        ## User CLIENT2
+        CLI2 = Entite.get_by(codent=u"CLIENT2")
+        u = User()
+        u.codusr = u"CLI2_ADM"
+        u.name = u"Admin CLIENT2"
+        u.entite = CLI2
+        u.IsAdmin = True
+        session.commit()
+        u = User()
+        u.codusr = u"CLI2_USR1"
+        u.name = u"USER2 CLIENT2"
+        u.entite = CLI2
+        u.IsAdmin = False
+        session.commit()
 
-setup_all(True)
-create_all()
+    def pop_fex(self):
+        f = Fex()
+        f.nom = u"FEX1"
+        f.description = u"1er echange de fichier"
+        f.status = u"CREE"
+        f.expediteur = User.get_by(codusr=u'SRA_USR1')
+        f.destinataire = User.get_by(codusr=u'CLI1_USR1')
+        session.commit()
 
-populate()
+        f = Fex()
+        f.nom = u"FEX2"
+        f.description = u"2eme echange de fichier"
+        f.status = u"CREE"
+        f.expediteur = User.get_by(codusr=u'CLI2_USR1')
+        f.destinataire = User.get_by(codusr=u'SRA_USR1')
+        session.commit()
+
+    def populate(self):
+        self.pop_entite()
+        self.pop_user()
+        self.pop_fex()
+
+    def init_base(self):
+        setup_all(True)
+        create_all()
+        self.populate()
+
+def test():
+    H = Hub('HUB1')
+    H.init_base()
+
+
+if __name__ == '__main__':
+    test()
--- fex/src/model.py
+++ fex/src/model.py
@@ -2,40 +2,40 @@ from elixir import *
 import datetime
 
 metadata.bind = "sqlite:///fex.dbf"
-metadata.bind.echo = True
+metadata.bind.echo = False
 
 class Entite(Entity):
-    using_options(tablename='ENTITE')
+    using_options(tablename=u'ENTITE')
     codent = Field(Unicode(10))
     nom = Field(Unicode(30))
     description = Field(UnicodeText, default=u"" )
-    typ = Field(Unicode(10), default='CLIENT')
-    parent = ManyToOne('Entite')
+    typ = Field(Unicode(10), default=u'CLIENT')
+    parent = ManyToOne(u'Entite')
     def __repr__(self):
         return '<Entite : %s : %s : %s>' % (self.codent, self.typ, self.parent)
 
 class User(Entity):
-    using_options(tablename='USER')
+    using_options(tablename=u'USER')
     codusr = Field(Unicode(10))
     name = Field(Unicode(30))
-    entite = ManyToOne('Entite')
+    entite = ManyToOne(u'Entite')
     email = Field(Unicode(20))
     IsAdmin = Field(Boolean, default=False)
     def __repr__(self):
         return '<User : %s : %s : %s>' % (self.codusr, self.entite, self.IsAdmin)
 
 class Fex(Entity):
-    using_options(tablename='FEX')
+    using_options(tablename=u'FEX')
     nom = Field(Unicode(30))
     description = Field(UnicodeText)
     status = Field(Unicode(10))
-    emetteur = ManyToOne('User')
-    destinataire = ManyToOne('User')
+    expediteur = ManyToOne(u'User')
+    destinataire = ManyToOne(u'User')
     date_creation = Field(DateTime, default=datetime.datetime.now)
     date_modif = Field(DateTime)
     date_depot = Field(DateTime)
-    date_reception = Field(DateTime)
+    date_retrait = Field(DateTime)
 
     def __repr__(self):
-        return '<File Exchange %s %s>' % (self.nom, self.status)
+        return '<File Exchange %s %s %s %s>' % (self.nom, self.status, self.expediteur, self.destinataire) 
 
